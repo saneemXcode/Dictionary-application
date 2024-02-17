48a828426af38eb0061c1e1b8d7bf6e3045b2447
saneemXcode <mahammadsaneem8310@gmail.com>
2024-02-17 20:18:36 +0530

    change

--- script.js
+++ script.js
@@ -1,133 +1,4 @@
-// const input =document.querySelector('input')
-// const btn=document.querySelector('button')
-// const dictionary=document.querySelector('.dictionary-app')
 
-// // https://api.dictionaryapi.dev/api/v2/entries/en/<word>
-
-// async function dictionaryFn(word){
-//     const res= await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)//in this word we passing text from keyword 
-//     // when we use async function we use await 
-//     .then(res => res.json())
-//     return res[0]
-// }
-
-// btn.addEventListener('click',fetchandCreateCard)
-
-// async function fetchandCreateCard(){
-//     const data= await dictionaryFn(input.value)
-//     console.log(data)
-
-//     let partOfSpeech=[]
-//     for(let i=0;i<data.meanings.length-1;i++){
-//         partOfSpeech.push(data.meanings[i].partOfSpeech)
-//     }
-
-
-// dictionary.innerHTML=`
-// <div class="card">
-// <div class="property">
-// <span>Word :</span>
-// <span>${data.word}</span>
-// </div>
-// <div class="property">
-// <span>Phonetics :</span>
-// <span>${data.phonetic}</span>
-// </div>
-// <div class="property">
-// <span>
-// <audio controls src="${data.phonetics[1].audio }"></audio>
-// </span>
-
-// </div>
-// <div class="property">
-// <span>Definition</span>
-// <span>${data.meanings[0].definitions[0].definition}</span>
-// </div>
-// <div class="property">
-// <span>Example:</span>
-// <span>${data.meanings[1].definitions[0].example} </span>
-// </div>
-// <div class="property">
-// <span>Part of Speech :</span>
-// <span>${partOfSpeech.map(e => e).join(',')}</span>
-// </div>
-// </div>
-// `
-
-// }
-
-// const input = document.querySelector('input');
-// const btn = document.querySelector('button');
-// const dictionary = document.querySelector('.dictionary-app');
-
-// // https://api.dictionaryapi.dev/api/v2/entries/en/<word>
-
-// async function dictionaryFn(word) {
-//     const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-//         .then(res => res.json());
-//     return res[0];
-// }
-
-// btn.addEventListener('click', fetchandCreateCard);
-// input.addEventListener('input', checkInputValue);
-
-// async function fetchandCreateCard() {
-//     const data = await dictionaryFn(input.value);
-//     console.log(data);
-
-//     let partOfSpeech = [];
-//     for (let i = 0; i < data.meanings.length - 1; i++) {
-//         partOfSpeech.push(data.meanings[i].partOfSpeech);
-//     }
-
-//     let allPhonetics = [];
-//     data.phonetics.forEach(phonetic => {
-//         allPhonetics.push(phonetic.text);
-//     });
-//     if (data.phonetics.length > 1) {
-//         allPhonetics = allPhonetics.join(', ');
-//     } else {
-//         allPhonetics = allPhonetics[0];
-//     }
-
-//     dictionary.innerHTML = `
-//         <div class="card">
-//             <div class="property">
-//                 <span>Word :</span>
-//                 <span>${data.word}</span>
-//             </div>
-//             <div class="property">
-//                 <span>Phonetics :</span>
-//                 <span>${allPhonetics}</span>
-//             </div>
-//             <div class="property">
-//                 <span>
-//                     <audio controls src="${data.phonetics[1].audio}"></audio>
-//                 </span>
-//             </div>
-//             <div class="property">
-//                 <span>Definition</span>
-//                 <span>${data.meanings[0].definitions[0].definition}</span>
-//             </div>
-//             <div class="property">
-//                 <span>Example:</span>
-//                 <span>${data.meanings[1].definitions[0].example}</span>
-//             </div>
-//             <div class="property">
-//                 <span>Part of Speech :</span>
-//                 <span>${partOfSpeech.join(',')}</span>
-//             </div>
-//         </div>
-//     `;
-// }
-
-// function checkInputValue() {
-//     if (input.value === '') {
-//         dictionary.innerHTML = ''; // Clear the content if input is empty
-//     }
-// }
-
-// last modified version code
 const input = document.querySelector('input');
 const btn = document.querySelector('button');
 const dictionary = document.querySelector('.dictionary-app');
@@ -154,29 +25,32 @@ async function fetchandCreateCard() {
     let example = '';
     let currentIndex = 0;
 
-    while (!(partOfSpeech.length && allPhonetics.length && audioSrc && definition && example) && currentIndex < data.meanings.length) {
-        if (!partOfSpeech.length && data.meanings[currentIndex]?.partOfSpeech) {
-            partOfSpeech.push(data.meanings[currentIndex].partOfSpeech);
+    while (currentIndex < data.meanings.length) {
+        const meaning = data.meanings[currentIndex];
+        if (!partOfSpeech.length && meaning?.partOfSpeech) {
+            partOfSpeech.push(meaning.partOfSpeech);
         }
-
-        if (!allPhonetics.length && data.phonetics[currentIndex]?.text) {
-            allPhonetics.push(data.phonetics[currentIndex].text);
+        
+        const phonetic = data.phonetics[currentIndex];
+        if (!allPhonetics.length && phonetic?.text) {
+            allPhonetics.push(phonetic.text);
         }
-
-        if (!audioSrc && data.phonetics[currentIndex]?.audio) {
-            audioSrc = data.phonetics[currentIndex].audio;
+    
+        if (!audioSrc && phonetic?.audio) {
+            audioSrc = phonetic.audio;
         }
-
-        if (!definition && data.meanings[currentIndex]?.definitions.length > 0) {
-            definition = data.meanings[currentIndex].definitions[0].definition;
+    
+        if (!definition && meaning?.definitions.length > 0) {
+            definition = meaning.definitions[0].definition;
         }
-
-        if (!example && data.meanings[currentIndex]?.definitions.length > 0) {
-            example = data.meanings[currentIndex].definitions[0].example;
+    
+        if (!example && meaning?.definitions.length > 0) {
+            example = meaning.definitions[0].example;
         }
-
+    
         currentIndex++;
     }
+    
 
     dictionary.innerHTML = `
         <div class="card">
